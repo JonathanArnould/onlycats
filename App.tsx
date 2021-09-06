b5c489b38884cb74e0ee7b6e86335aaef60839db
alexis-roger-newmips-com <alexis.roger@newmips.com>
2021-09-06 11:18:02 +0200

    added picture button

--- App.tsx
+++ App.tsx
@@ -1,18 +1,48 @@
 import React from 'react';
-import { NavigationContainer } from "@react-navigation/native";
+import { useColorScheme } from 'react-native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { StyleSheet, Text, View } from 'react-native';
+import Ionicons from "@expo/vector-icons/Ionicons";
 import CameraScreen from "./screens/CameraScreen";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
+    <NavigationContainer theme={MyTheme}>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Camera") {
+              iconName = focused ? "camera" : "camera-outline";
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "white",
+          tabBarInactiveTintColor: "white",
+        })}
+      >
         <Tab.Screen name="Camera" component={CameraScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
+
+
+const MyTheme = {
+  dark: false,
+  colors: {
+    primary: 'rgb(12, 0, 0)',
+    background: 'rgb(12, 0, 0)',
+    card: 'rgb(12, 0, 0)',
+    text: 'rgb(28, 28, 30)',
+    border: 'rgb(12, 0, 0)',
+    notification: 'rgb(255, 69, 58)',
+  },
+};
 
 const styles = StyleSheet.create({
   container: {
--- screens/CameraScreen.tsx
+++ screens/CameraScreen.tsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
 import { Camera } from "expo-camera";
 
 export default function CameraScreen() {
   const [hasPermission, setHasPermission] = useState(null);
+  const buttonClickedHandler = () => {
+    console.log('You have been clicked a button!');
+    // do something
+  };
 
   useEffect(() => {
     (async () => {
@@ -21,7 +25,11 @@ export default function CameraScreen() {
   return (
     <>
       <Camera style={styles.camera} />
-      <Button title="Press me" onPress={() => console.log("Pressed")} />
+      <TouchableOpacity
+        onPress={buttonClickedHandler}
+        style={styles.picture}>
+        <Text></Text>
+      </TouchableOpacity>
     </>
   );
 }
@@ -30,4 +38,18 @@ const styles = StyleSheet.create({
   camera: {
     flex: 1,
   },
+  picture: {
+    borderStyle: 'solid',
+    borderColor: 'white',
+    borderWidth: 1,
+    position: 'absolute',
+    bottom: 15,
+    width: 60,
+    height: 60,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    padding: 10,
+    borderRadius: 100,
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+  }
 });
