6011ebac22c39adcf4f15488f93a858462ef001c
ahamel1 <ahamel.wcs@gmail.com>
2021-09-07 11:55:36 +0200

    typage and resoslve nesting navigator

--- App.tsx
+++ App.tsx
@@ -1,15 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 import Navbar from "./components/Navbar";
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from "expo-status-bar";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import PublishScreen from "./screens/PublishScreen";
 
-const Tab = createBottomTabNavigator();
+// const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Navbar />
+    <NavigationContainer theme={MyTheme}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={Navbar}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="PublishScreen" component={PublishScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
@@ -17,8 +30,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     margin: 0,
     padding: 0,
   },
 });
+
+const MyTheme = {
+  dark: false,
+  colors: {
+    primary: 'rgb(12, 0, 0)',
+    background: 'rgb(12, 0, 0)',
+    card: 'rgb(12, 0, 0)',
+    text: 'rgb(28, 28, 30)',
+    border: 'rgb(12, 0, 0)',
+    notification: 'rgb(255, 69, 58)',
+  },
+};
--- screens/CameraScreen.tsx
+++ screens/CameraScreen.tsx
@@ -1,33 +1,44 @@
 import React, { useState, useEffect, useRef } from "react";
-import { StyleSheet, Image, Text, View, Platform, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Image,
+  Text,
+  View,
+  Platform,
+  TouchableOpacity,
+} from "react-native";
 import { Camera } from "expo-camera";
-import * as ImagePicker from 'expo-image-picker';
+import * as ImagePicker from "expo-image-picker";
 import * as ImageManipulator from "expo-image-manipulator";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { useIsFocused } from '@react-navigation/native';
+import { ParamListBase, useIsFocused, useNavigation } from "@react-navigation/native";
+import {StackNavigationProp} from '@react-navigation/stack';
 
+type CameraScreenProps = StackNavigationProp<ParamListBase, 'Camera'>;
 
-export default function CameraScreen({ navigation}) {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [image, setImage] = useState(null);
-  const cameraRef = useRef(null);
+export default function CameraScreen() {
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+  const [image, setImage] = useState<string>("");
+  const cameraRef = useRef<any>(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
   const isFocused = useIsFocused();
 
+  const navigation = useNavigation<CameraScreenProps>();
+
   const buttonClickedHandler = async () => {
-      const pictureMetadata = await cameraRef.current.takePictureAsync();
-      // console.log("pictureMetadata", pictureMetadata);
-      navigation.navigate('PublishScreen', {
-        picture : pictureMetadata
-      });
+    const pictureMetadata = await cameraRef.current.takePictureAsync();
+    navigation.navigate("PublishScreen", {
+      picture: pictureMetadata,
+    });
   };
 
   useEffect(() => {
     (async () => {
-      if (Platform.OS !== 'web') {
-        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-        if (status !== 'granted') {
-          alert('Sorry, we need camera roll permissions to make this work!');
+      if (Platform.OS !== "web") {
+        const { status } =
+          await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== "granted") {
+          alert("Sorry, we need camera roll permissions to make this work!");
         }
       }
     })();
@@ -36,7 +47,7 @@ export default function CameraScreen({ navigation}) {
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
+      setHasPermission(status === "granted");
     })();
   }, []);
 
@@ -49,7 +60,6 @@ export default function CameraScreen({ navigation}) {
     });
 
     console.log(result);
-
 
     if (!result.cancelled) {
       setImage(result.uri);
@@ -64,29 +74,34 @@ export default function CameraScreen({ navigation}) {
   }
   return (
     <>
-      { isFocused && <Camera style={styles.camera} type={type} ref={cameraRef}/>  }
-      
+      {isFocused && (
+        <Camera style={styles.camera} type={type} ref={cameraRef} />
+      )}
+
       <TouchableOpacity
         onPress={buttonClickedHandler}
-        style={styles.picture}>
-      </TouchableOpacity>
+        style={styles.picture}
+      ></TouchableOpacity>
 
       <TouchableOpacity
         style={styles.flip}
         onPress={() => {
-            setType(
+          setType(
             type === Camera.Constants.Type.back
-                ? Camera.Constants.Type.front
-                : Camera.Constants.Type.back
-            );
-        }}>
-          <Text><Ionicons name={"camera-reverse-outline"} size={30} color={"white"} /></Text>
+              ? Camera.Constants.Type.front
+              : Camera.Constants.Type.back
+          );
+        }}
+      >
+        <Text>
+          <Ionicons name={"camera-reverse-outline"} size={30} color={"white"} />
+        </Text>
       </TouchableOpacity>
 
-      <TouchableOpacity
-        onPress={pickImage}
-        style={styles.fileupload}>
-        <Text><Ionicons name={"duplicate-outline"} size={30} color={"white"}/></Text>
+      <TouchableOpacity onPress={pickImage} style={styles.fileupload}>
+        <Text>
+          <Ionicons name={"duplicate-outline"} size={30} color={"white"} />
+        </Text>
       </TouchableOpacity>
     </>
   );
@@ -97,31 +112,31 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   picture: {
-    borderStyle: 'solid',
-    borderColor: 'white',
+    borderStyle: "solid",
+    borderColor: "white",
     borderWidth: 1,
-    position: 'absolute',
+    position: "absolute",
     bottom: 15,
     width: 60,
     height: 60,
-    justifyContent: 'center',
-    alignSelf: 'center',
+    justifyContent: "center",
+    alignSelf: "center",
     padding: 10,
     borderRadius: 100,
-    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    backgroundColor: "rgba(255, 255, 255, 0.2)",
   },
   fileupload: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 15,
     left: 15,
   },
   flip: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 15,
     right: 15,
   },
   text: {
     fontSize: 18,
-    color: 'white',
+    color: "white",
   },
 });
