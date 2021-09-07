9bb30a9669b45641f6d22e1f1635a9086018d7e0
auxb <auxence.blondel@gmail.com>
2021-09-07 14:25:58 +0200

    add cat fake data

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -1,8 +1,31 @@
 import React, { useState, useEffect } from "react";
 import MapView from "react-native-maps";
 import { Marker } from "react-native-maps";
-import { StyleSheet, View, Dimensions, Alert } from "react-native";
+import { StyleSheet, View, Dimensions, Alert, Image } from "react-native";
 import * as Location from "expo-location";
+
+const cats = [
+  {
+    id: "1234",
+    name: "kiwi",
+    coordinate: {
+      latitude: 44.84132461780238,
+      longitude: -0.5701245711323272,
+    },
+    image:
+      "https://www.i-cad.fr/uploads/Connaitre_chat.jpg",
+  },
+  {
+    id: "4567",
+    name: "simba",
+    coordinate: {
+      latitude: 44.83427724121175,
+      longitude: -0.5649559458391263,
+    },
+    image:
+      "https://cdn.lelynx.fr/wp-content/uploads/2020/05/GettyImages-1214907564.jpg",
+  },
+];
 
 export default function MapScreen() {
   const [userLocation, setUserLocation] = useState({
@@ -19,7 +42,7 @@ export default function MapScreen() {
         [
           {
             text: "Retry",
-            onPress: async() => await requestLocationPermission(),
+            onPress: async () => await requestLocationPermission(),
             style: "cancel",
           },
           { text: "OK" },
@@ -28,6 +51,8 @@ export default function MapScreen() {
       return;
     }
   };
+
+  const PICTURE_SIZE = 1;
 
   useEffect(() => {
     (async () => {
@@ -46,9 +71,30 @@ export default function MapScreen() {
     <View style={styles.container}>
       <MapView
         style={styles.map}
-        region={{ ...userLocation, latitudeDelta: 0, longitudeDelta: 0 }}
+        initialRegion={{
+          ...userLocation,
+          latitudeDelta: 10,
+          longitudeDelta: 45,
+        }}
       >
         <Marker coordinate={userLocation} />
+        {cats.map((cat, id) => {
+          return (
+            <Marker
+              title={cat.name}
+              key={id}
+              coordinate={cat.coordinate}
+              image={{ uri: "custom" }}
+            >
+              <View style={styles.marker}>
+                <Image
+                  source={{ uri: cat.image }}
+                  style={styles.image}
+                />
+              </View>
+            </Marker>
+          );
+        })}
       </MapView>
     </View>
   );
@@ -64,5 +110,15 @@ const styles = StyleSheet.create({
   map: {
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height,
+  },
+  image: {
+    borderRadius: 100,
+    width: 50,
+    height: 50
+  },
+  marker: {
+    backgroundColor: "blue",
+    padding: 2,
+    borderRadius: 100,
   },
 });
