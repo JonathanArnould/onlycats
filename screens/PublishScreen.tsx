b6300f961fa08c07c9b40713f11f88af13740efe
alexis-roger-newmips-com <alexis.roger@newmips.com>
2021-09-09 14:15:56 +0200

    sac a merde

--- screens/PublishScreen.tsx
+++ screens/PublishScreen.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Image, StyleSheet, Text, View, TextInput, Button, ScrollView } from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack/lib/typescript/src/types";
 import { useForm, Controller } from "react-hook-form";
+import {Picker} from '@react-native-picker/picker';
 
 type RootStackParamList = {
   PublishScreen: any;
@@ -12,6 +13,8 @@ type Props = NativeStackScreenProps<RootStackParamList, "PublishScreen">;
 export default function ImagesScreen({ route }: Props) {
   const { control, handleSubmit, formState: { errors } } = useForm();
   const onSubmit = data => console.log(data);
+  const [selectedBreed, setSelectedBreed] = useState();
+  const [selectedCategory, setSelectedCategory] = useState();
   return (
     <>
       <ScrollView>
@@ -23,7 +26,7 @@ export default function ImagesScreen({ route }: Props) {
           source={{ uri: route.params !== undefined && route.params.picture.uri }}
         />
         <View style={styles.container}>
-        <Text style={styles.label}>Nom</Text>
+          <Text style={styles.label}>Nom</Text>
           <Controller
             control={control}
             rules={{
@@ -42,49 +45,51 @@ export default function ImagesScreen({ route }: Props) {
             defaultValue=""
           />
           {errors.Name && <Text>This is required.</Text>}
-          <View style={styles.fixToText}>
             <Text style={styles.label}>Race</Text>
-            <Text style={styles.label}>Catégorie</Text>
-
-          </View>
-          <View style={styles.fixToText}>
             <Controller
               control={control}
               rules={{
               maxLength: 100,
               }}
               render={({ field: { onChange, onBlur, value } }) => (
-                <TextInput
-                  style={styles.input}
-                  onBlur={onBlur}
-                  onChangeText={onChange}
-                  value={value}
-                  placeholder={'#breed'}
-                />
+              <Picker
+                style={styles.input}
+                prompt={'Race du chat'}
+                selectedValue={selectedBreed}
+                onValueChange={(itemValue, itemIndex) =>
+                  setSelectedBreed(itemValue)
+                }>
+                <Picker.Item label="Maine Coon" value="mainCoon" />
+                <Picker.Item label="Bengal" value="bengal" />
+                <Picker.Item label="Siamois" value="siamois" />
+                <Picker.Item label="Munchkin" value="munchkin" />
+              </Picker>
               )}
               name="Race"
               defaultValue=""
             />
-
+            <Text style={styles.label}>Catégorie</Text>
             <Controller
               control={control}
               rules={{
               maxLength: 100,
               }}
               render={({ field: { onChange, onBlur, value } }) => (
-                <TextInput
-                  style={styles.input}
-                  onBlur={onBlur}
-                  onChangeText={onChange}
-                  value={value}
-                  placeholder={'#category'}
-                />
+                <Picker
+                style={styles.input}
+                prompt={'Catégorie'}
+                selectedValue={selectedCategory}
+                onValueChange={(itemValue, itemIndex) =>
+                  setSelectedCategory(itemValue)
+                }>
+                <Picker.Item label="Funny" value="funny" />
+                <Picker.Item label="Cute" value="cute" />
+                <Picker.Item label="Sleeping" value="sleeping" />
+              </Picker>
               )}
               name="Category"
               defaultValue=""
             />
-          </View>
-
           <Text style={styles.label}>Adresse</Text>
           <Controller
             control={control}
@@ -120,6 +125,27 @@ export default function ImagesScreen({ route }: Props) {
               />
             )}
             name="City"
+            defaultValue=""
+          />
+
+          <Text style={styles.label}>Description</Text>
+          <Controller
+            control={control}
+            rules={{
+            maxLength: 100,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                multiline
+                numberOfLines={4}
+                style={styles.input}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                placeholder={'Description'}
+              />
+            )}
+            name="Description"
             defaultValue=""
           />
           <Button title="Submit" onPress={handleSubmit(onSubmit)} color="#841584" />
